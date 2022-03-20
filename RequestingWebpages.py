0584c4842c614e528062a447c86e831a68a880a7
Jeremiah Quaynor <73517688+Jeremiah-Quaynor@users.noreply.github.com>
2022-03-20 18:40:16 +0000

    Add files via upload
    adding functionality

--- RequestingWebpages.py
+++ RequestingWebpages.py
@@ -4,3 +4,11 @@ google = requests.get("https://www.google.com")
 
 if google.status_code != 200:
     print("Error encountered, please restart your code")
+else:
+    print("Youu requested a webapage on " + google.headers["Date"])
+    print("Your webpage will be downloaded shortly")
+    page = google.text
+
+    fil = open("index.html", "w")
+    fil.write(page)
+    fil.close()
